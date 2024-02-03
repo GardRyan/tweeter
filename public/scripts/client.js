54c6b978de21a81ede1427eaf760f3a5065a721a
GardRyan <res.gardner@gmail.com>
2024-02-02 19:16:03 -0500

    error messages!

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -43,31 +43,54 @@ $(document).ready(function () {
     //prevent the default behaviour (refresh)
 
     event.preventDefault();
-   
+
     //serialize form data
     const tweetText = $("#tweet-text").val();
     const sanitizedText = $("<p>").text(tweetText).html();
-       
+
     if (sanitizedText === null) {
-      alert(`We're going to need a bigger boat!`);
+      displayMessage(
+        `We're going to need a bigger boat! Or at least a caption... `,
+        "error"
+      );
       return;
     }
     if (sanitizedText.length > 140) {
-      alert(`We're going to need a smaller boat!`);
+      displayMessage(
+        `We're not writing the entire LOTR, Bilbo! Let's just start with "The Hobbit", if it were 140 characters`,
+        "error"
+      );
       return;
     }
     console.log(sanitizedText);
-   
 
     //AJAX post req in client.js that send form to server
-    $.post("/tweets", {text: sanitizedText}, function (response) {
+    $.post("/tweets", { text: sanitizedText }, function (response) {
       console.log(response);
       const $tweetContainer = $("#tweet-container");
       $tweetContainer.empty();
-      $("#tweet-text").val('');
+      $("#tweet-text").val("");
       loadTweets();
     });
   });
+
+  const displayMessage = function (message, messageType) {
+    const $messageContainer = $(".message-container");
+    const $message = $messageContainer.find("p");
+  
+    $message.text(message).addClass(messageType);
+    console.log($message);
+
+    if ($messageContainer.is(":hidden")) {
+      $messageContainer.slideDown();
+    }
+
+    setTimeout(function () {
+      $messageContainer.slideUp(function() {
+        $message.text('').removeClass(messageType);
+      });
+    }, 5000);
+  };
 
   const loadTweets = function () {
     $.ajax({
